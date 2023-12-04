3f4d0a05488bcb18bbf4e1d129837eaa8c7e3eea
jkropko <jkropko@gmail.com>
2023-12-04 16:21:50 -0500

    Enabled loop through all available posts

--- helper.py
+++ helper.py
@@ -18,8 +18,7 @@ def gettoken():
     return json.loads(r.text)['access_token']
 
 root = 'https://open.tiktokapis.com/v2/research'
-headers = {'Authorization': 'Bearer ' + gettoken(),
-          'Content-Type': 'application/json'}
+
 fields = ['id',
          'video_description',
          'create_time', 
@@ -37,7 +36,7 @@ fields = ['id',
 params = {'fields': ','.join(fields)}
 
 def queryvideo(query, start_date, end_date,
-               root=root, headers=headers, max_count=100,
+                headers, root=root, max_count=100,
                params=params):
     endpoint = '/video/query/'
     data = {'query': json.dumps(json.loads(query)),
@@ -47,10 +46,34 @@ def queryvideo(query, start_date, end_date,
     r = requests.post(root+endpoint, headers=headers, params=params, data=data)
     myjson = json.loads(r.text)
     try:
-        return (myjson['data']['cursor'],
-           myjson['data']['has_more'],
-           myjson['data']['search_id'],
-           pd.json_normalize(myjson, record_path = ['data', 'videos']))
+        cursor = myjson['data']['cursor']
+        has_more = myjson['data']['has_more']
+        search_id = myjson['data']['search_id']
+        df = pd.json_normalize(myjson, record_path = ['data', 'videos'])
+        while has_more:
+            print(cursor)
+            data = {'query': json.dumps(json.loads(query)),
+                    'start_date': start_date,
+                    'end_date': end_date,
+                    'max_count': max_count,
+                    'cursor': cursor,
+                    'search_id': search_id}
+            r = requests.post(root+endpoint, 
+                             headers=headers, 
+                             params=params, 
+                             data=data)
+            myjson = json.loads(r.text)
+            cursor = myjson['data']['cursor']
+            has_more = myjson['data']['has_more']
+            print(has_more)
+            search_id = myjson['data']['search_id']
+            df = pd.concat(
+                [df, 
+                 pd.json_normalize(myjson, record_path = ['data', 'videos'])
+                ])
+        return df
     except:
-        return (r.text, data)
+        return (r.text, r.url, data)
+
+    
     
